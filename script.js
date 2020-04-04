d1895e22bc4e1bb4ee448b9be4f18dd4160c775e
Camilo Calvache <camilo.calvache@gmail.com>
2020-04-03 19:40:54 -0500

    codigo de la calculadora

---
+++ script.js
@@ -0,0 +1,25 @@
+var one = document.getElementById("one");
+var two = document.getElementById("two");
+var three = document.getElementById("three");
+var four = document.getElementById("four");
+var five = document.getElementById("five");
+var six = document.getElementById("six");
+var seven = document.getElementById("seven");
+var eight = document.getElementById("eight");
+var nine = document.getElementById("nine");
+var zero = document.getElementById("zero");
+
+var plus = document.getElementById("plus");
+var minus = document.getElementById("minus");
+var times = document.getElementById("times");
+var div = document.getElementById("div");
+var equals = document.getElementById("equals");
+var clear = document.getElementById("clear");
+
+
+function calculate() {
+
+    one = onclick = console.log = +1;
+
+
+}
